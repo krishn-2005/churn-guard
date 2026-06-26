c1fbe40ea1422fb111e107a79c0182b14eff994f
Krishna Prajapati <krishnaprajapat8814@outlook.com>
2026-06-26 18:11:28 +0530

    Update app.py

--- Web/Backend/app.py
+++ Web/Backend/app.py
@@ -83,6 +83,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3001",
+        "https://churn-guard-7tlb.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
